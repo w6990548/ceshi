a5d72acb36e31b579675b0d69ba33a75ea77ec47
FengLei <www.409050716@foxmail.com>
2020-05-18 18:21:52 +0800

    初始化项目，laravel + vue Demo

---
+++ resources/js/components/Hello.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>Hello, Word!</h1>
+        <p class="hello">{{ msg }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                msg: 'This is a Laravel with Vue Demo.'
+            }
+        }
+    }
+</script>
+
+<style>
+    .hello {
+        font-size: 2em;
+        color: green;
+    }
+</style>
